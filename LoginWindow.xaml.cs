17a3c3df9c0eceba5024480838ebc8aeb9b97d0b
lawlietl4 <nroberson7712@gmail.com>
2021-05-25 15:41:55 -0600

    Add project files.

---
+++ LoginWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using FinalFantasy7ItemManager.DataSet1TableAdapters;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace FinalFantasy7ItemManager
+{
+    /// <summary>
+    /// Meant to allow people to login if they exist in the database
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        private string username;
+        private string password;
+        public string Username
+        {
+            get { return username; }
+            set => username = value;
+        }
+        public string Password
+        {
+            get { return password; }
+            set => password = value;
+        }
+        private readonly DataSet1 DataSet = new();
+        public UsersTableAdapter userTableAdapter = new();
+
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            userTableAdapter.Fill(DataSet.Users);
+
+        }
+        /// <summary>
+        /// specifically for logging and if the user doesn't exist give an error and try again
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from user in DataSet.Users
+                        where (user.Name == UsernameInput.Text)
+                        where (user.Password == PasswordInput.Text)
+                        select user;
+            if (query.Any())
+            {
+                MainWindow mainWindow = new();
+                mainWindow.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("User does not exist!");
+                UsernameInput.Clear();
+                PasswordInput.Clear();
+            }
+        }
+        /// <summary>
+        /// add users to database if you need a new user
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RegisterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataSet1.UsersRow row = (DataSet1.UsersRow)DataSet.Users.NewRow();
+            row.Name = UsernameInput.Text;
+            row.Password = PasswordInput.Text;
+            DataSet.Users.AddUsersRow(row);
+            userTableAdapter.Update(DataSet);
+            MessageBox.Show("User has been successfully registered!");
+            UsernameInput.Clear();
+            PasswordInput.Clear();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using FinalFantasy7ItemManager.DataSet1TableAdapters;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace FinalFantasy7ItemManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private enum EState
+        {
+            idle,
+            clicked
+        }
+        EState state = EState.idle;
+        private readonly ItemsTableAdapter itemTableAdapter = new();
+        private readonly DataSet1 dataSet = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            itemTableAdapter.Fill(dataSet.Items);
+            dataGrid.DataContext = dataSet.Items.DefaultView;
+        }
+
+        private void UpdateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            state = EState.clicked;
+            buttonHasBeenClicked btnEvent = new();
+            btnEvent.ChocoboTime();
+        }
+    }
+
+    public partial class buttonHasBeenClicked : INotifyPropertyChanged
+    {
+        private readonly Uri Path = new(@"pack://siteoforigin:,,,/Resources/hip_hop_de_chocobo.mp3");
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private readonly ItemsTableAdapter itemTableAdapter = new();
+        private readonly DataSet1 dataSet = new();
+        private readonly MediaPlayer soundPlayer = new();
+
+        private enum EState
+        {
+            idle,
+            clicked
+        }
+
+        private EState state = EState.idle;
+
+        public void ChocoboTime()
+        {
+            if (state == EState.clicked)
+            {
+                itemTableAdapter.Update(dataSet);
+                soundPlayer.Open(Path);
+                soundPlayer.Play();
+                state = EState.idle;
+            }
+        }
+    }
+}
